e248a6e32ccb0bb632213e85a3fe0a2dbd0e28ae
naseokju <lukas95@naver.com>
2023-04-28 15:38:06 +0900

    6.16 Final Project part Two

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
@@ -38,25 +38,24 @@ const Overlay = styled(motion.div)`
 `;
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggle = () => setClicked((prev) => !prev);
+  const [id, setId] = useState<null | string>(null);
   
   return (
-    <Wrapper onClick={toggle}>
+    <Wrapper>
       <Grid>
-        <Box layoutId="hello" />
-        <Box />
-        <Box />
-        <Box />
+        {["1", "2", "3", "4"].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
       </Grid>
       <AnimatePresence> 
-        {clicked ? (
+        {id ? (
           <Overlay 
+            onClick={() => setId(null)}
             initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }} 
-            animate={{ backgroundColor: "rgba(0, 0, 0, 1)" }} 
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }} 
             exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }} 
           >
-            <Box layoutId="hello" style={{ width: 400, height: 200 }} />
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
           </Overlay>
         ) : null}
       </AnimatePresence>
